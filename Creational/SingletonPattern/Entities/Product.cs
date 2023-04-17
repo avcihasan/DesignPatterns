80db3dcb3d7d31fbab45434d64a2d24be4f57504
Hasan Avcı <hsnavci7@gmail.com>
2023-04-17 21:17:33 +0300

    SingletonPattern

---
+++ Creational/SingletonPattern/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace SingletonPattern.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        static Product _instance;
+        private Product(){}
+        static Product()
+        {
+            _instance=new Product() { Name="Kalem",Price=20};
+        }
+        static public Product GetInstance()
+            => _instance;
+
+        //public static Product GetInstance
+        //{
+        //    get
+        //    {
+        //        if (_instance == null)
+        //            _instance = new Product();
+        //        return _instance;
+        //    }
+        //}
+
+    }
+}
---
+++ Creational/SingletonPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingletonPattern.Entities;
+
+Product p = Product.GetInstance();
+Product p2 = Product.GetInstance2;
+
+Console.WriteLine($"Name : {p.Name} \nPrice : {p.Price}");
+Console.WriteLine($"Name : {p2.Name} \nPrice : {p2.Price}");
+
