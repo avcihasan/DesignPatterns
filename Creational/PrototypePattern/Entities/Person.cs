6be3c1eaf2f77ed844ba1664abfccc13b53900e0
Hasan Avcı <hsnavci7@gmail.com>
2023-05-27 01:59:25 +0300

    PrototypePattern

---
+++ Creational/PrototypePattern/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.Entities
+{
+    public class Person : ICloneable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public object Clone()
+        {
+            return base.MemberwiseClone();
+        }
+    }
+}
---
+++ Creational/PrototypePattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using PrototypePattern.Entities;
+
+Person person1 = new Person()
+{
+    Id= 1,
+    Name="Hasan",
+    Surname="Avcı"
+};
+
+Person person2 = (Person)person1.Clone();
+
+person2.Name = "Deneme Ad";
+person2.Surname = "Deneme Soyad";
+
+Person person3 = person1;
+person3.Name = "HASAN";
+
+Console.WriteLine($"Name :{person1.Name} - Surname : {person1.Surname} - Id : {person1.Id}");
+Console.WriteLine($"Name :{person2.Name} - Surname : {person2.Surname} - Id : {person2.Id}");
+Console.WriteLine($"Name :{person3.Name} - Surname : {person3.Surname} - Id : {person3.Id}");
+
+
+//person 1 ve person3 aynı objeyi referans ederken person2 yani person1 in klonu başka bir objeyi referans eder
