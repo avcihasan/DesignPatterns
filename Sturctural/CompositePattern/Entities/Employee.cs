ab8e8987c0e7d07ca36201c24ae9525753f530dd
Hasan Avcı <hsnavci7@gmail.com>
2023-05-29 16:00:01 +0300

    CompositePattern

---
+++ Sturctural/CompositePattern/Entities/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern.Entities
+{
+    public class Employee : IPerson,IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new();
+
+        public void AddSubordinates(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+        public void RemoveSubordinates(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+        public IPerson GetSubordinates(int index)
+        {
+            return _subordinates[index];
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var item in _subordinates)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Sturctural/CompositePattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using CompositePattern.Entities;
+
+Employee p1 = new() { Name = "deneme1" };
+Employee p2 = new() { Name = "deneme2" };
+Employee p3 = new() { Name = "deneme3" };
+Employee p4 = new() { Name = "deneme4" };
+Employee p5 = new() { Name = "deneme5" };
+Employee p6 = new() { Name = "deneme6" };
+
+p1.AddSubordinates(p2);
+p1.AddSubordinates(p3);
+p2.AddSubordinates(p3);
+p2.AddSubordinates(p4);
+p2.AddSubordinates(p5);
+p3.AddSubordinates(p6);
+p4.AddSubordinates(p5);
+p4.AddSubordinates(p6);
+p1.AddSubordinates(p6);
+
+Console.WriteLine(p1.Name);
+foreach (Employee item in p1)
+{
+    Console.WriteLine("  {0}", item.Name);
+    foreach (Employee item1 in item)
+    {
+        Console.WriteLine("    {0}", item1.Name);
+        foreach (Employee item2 in item1)
+        {
+            Console.WriteLine("      {0}", item2.Name);
+        }
+    }
+
+}
