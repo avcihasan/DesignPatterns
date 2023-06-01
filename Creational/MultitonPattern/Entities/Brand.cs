abe8f17d152377d08ac81c6aba2a0d909acbb28b
Hasan Avcı <hsnavci7@gmail.com>
2023-06-01 23:42:36 +0300

    MultitonPattern

---
+++ Creational/MultitonPattern/Entities/Brand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultitonPattern.Entities
+{
+    public class Brand
+    {
+        public Guid Id { get; set; } 
+        private static Dictionary<string, Brand> _brands;
+
+        private Brand()
+        {
+            Id=Guid.NewGuid();
+        }
+        static Brand()
+        {
+            _brands=new Dictionary<string, Brand>();
+        }
+
+        public static Brand GetBrand(string brand)
+        {
+            if (!_brands.ContainsKey(brand))
+                _brands.Add(brand, new Brand());
+            return _brands[brand];
+        }
+    }
+}
---
+++ Creational/MultitonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using MultitonPattern.Entities;
+
+Brand brand1 = Brand.GetBrand("Nike");
+Brand brand2 = Brand.GetBrand("Nike");
+Brand brand3 = Brand.GetBrand("Puma");
+Brand brand4 = Brand.GetBrand("Puma");
+
+Console.WriteLine(brand1.Id);
+Console.WriteLine(brand2.Id);
+Console.WriteLine(brand3.Id);
+Console.WriteLine(brand4.Id);
+
+Console.ReadLine();
